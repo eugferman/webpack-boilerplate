0d979ec83686fbd947b27034cb69ad08db2a43a1
eugferman <eugferman@gmail.com>
2020-04-27 16:03:04 +0200

    test sum added

--- src/sum.test.js
+++ src/sum.test.js
@@ -3,3 +3,7 @@ const suma = require('./sum');
 test('sumar 1 + 2 es igual a 3', () => {
   expect(suma(1, 2)).toBe(3);
 });
+
+test('sumar 4 + 4 es igual a 8', () => {
+  expect(suma(4, 4)).toBe(8);
+});
